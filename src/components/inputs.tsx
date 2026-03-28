5d2c8d699aeb57e8c2479ea105158976171c1c80
Pearl Darkowaa Yeboah [Head Office - Digital Transformation] <pdyeboah@gcb.com.gh>
2026-03-28 14:58:34 +0000

    added input validation

--- src/components/inputs.tsx
+++ src/components/inputs.tsx
@@ -1,9 +1,22 @@
-import type { InvestmentInput } from "../util/investment";
+import type { InvestmentFormInput } from "../util/investment";
+
+const countries = [
+  "Germany",
+  "United Kingdom",
+  "Ghana",
+  "United States"
+];
 
 type InputsProps = {
-  onChange: (inputIdentifier: keyof InvestmentInput, newValue: string) => void;
-  userInput: InvestmentInput;
+  onChange: (inputIdentifier: keyof InvestmentFormInput, newValue: string) => void;
+  userInput: InvestmentFormInput;
 };
+
+function handleNumberKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+  if (event.key === "e" || event.key === "E") {
+    event.preventDefault();
+  }
+}
 
 export default function Inputs({ onChange, userInput }: InputsProps) {
   return (
@@ -16,6 +29,7 @@ export default function Inputs({ onChange, userInput }: InputsProps) {
             type="number"
             required
             value={userInput.initialInvestment}
+            onKeyDown={handleNumberKeyDown}
             onChange={(event) =>
               onChange("initialInvestment", event.target.value)
             }
@@ -29,6 +43,7 @@ export default function Inputs({ onChange, userInput }: InputsProps) {
             type="number"
             required
             value={userInput.annualInvestment}
+            onKeyDown={handleNumberKeyDown}
             onChange={(event) =>
               onChange("annualInvestment", event.target.value)
             }
@@ -44,6 +59,7 @@ export default function Inputs({ onChange, userInput }: InputsProps) {
             type="number"
             required
             value={userInput.expectedReturn}
+            onKeyDown={handleNumberKeyDown}
             onChange={(event) => onChange("expectedReturn", event.target.value)}
           />
         </p>
@@ -55,8 +71,26 @@ export default function Inputs({ onChange, userInput }: InputsProps) {
             type="number"
             required
             value={userInput.duration}
+            onKeyDown={handleNumberKeyDown}
             onChange={(event) => onChange("duration", event.target.value)}
           />
+        </p>
+      </div>
+
+      <div className="input-group">
+        <p>
+          <label>Country</label>
+          <select
+            required
+            value={userInput.country}
+            onChange={(event) => onChange("country", event.target.value)}
+          >
+            {countries.map((country) => (
+              <option key={country} value={country}>
+                {country}
+              </option>
+            ))}
+          </select>
         </p>
       </div>
     </section>
